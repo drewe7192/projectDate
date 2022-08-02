bca1e90432bbec84e41dbe8b2fc68540291b07cf
drewe7192 <drewe7192@gmail.com>
2022-08-02 03:59:13 -0400

    Initial Commit

---
+++ projectDate/projectDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectDateApp.swift
+//  projectDate
+//
+//  Created by Drew Sutherlan on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct projectDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
