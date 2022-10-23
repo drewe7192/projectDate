7b23b7ef082ae78927abaebe162092b07aedbaf1
drewe7192 <drewe7192@gmail.com>
2022-10-23 04:11:25 -0400

    preview Profile screen Ui about complete

--- projectDate/views/screen/PreviewProfileView.swift
+++ projectDate/views/screen/PreviewProfileView.swift
@@ -11,73 +11,90 @@ import SwiftUI
 
 struct PreviewProfileView: View {
     var body: some View {
-        ScrollView{
-                Spacer()
-            AsyncImage(url: URL(string: "https://i.pinimg.com/236x/af/1c/30/af1c30d6d881d9447dec06149f61d2f9--drawings-of-girls-anime-drawings-girl.jpg")) { image in
-                image.resizable()
-                    .overlay(ImageOverlay())
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 600, height: 600)
-            .background(Color.gray)
-            .clipShape(Rectangle())
-       
-
-            
-           
-            AsyncImage(url: URL(string: "https://i.pinimg.com/236x/af/1c/30/af1c30d6d881d9447dec06149f61d2f9--drawings-of-girls-anime-drawings-girl.jpg")) { image in
-                image.resizable()
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 600, height: 600)
-            .background(Color.gray)
-            .clipShape(Rectangle())
-
-            AsyncImage(url: URL(string: "https://i.pinimg.com/236x/af/1c/30/af1c30d6d881d9447dec06149f61d2f9--drawings-of-girls-anime-drawings-girl.jpg")) { image in
-                image.resizable()
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 600, height: 600)
-            .background(Color.gray)
-            .clipShape(Rectangle())
-
-            
-            Section{
-                Text("Basics")
-                    .font(.subheadline)
-                HStack{
-                    Text("6'0")
-                    Text("active")
+        GeometryReader{geo in
+            ScrollView{
+                VStack{
+                    let width = geo.size.width
+                    let height = geo.size.height
+                    
+                    //Image
+                    AsyncImage(url: URL(string: "https://i.pinimg.com/originals/01/c6/f4/01c6f460f860a0c5d6a6c22d01716951.jpg")) { image in
+                        image.resizable()
+                            .overlay(ImageOverlay())
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: width, height: height)
+                    .background(Color.gray)
+                    .clipShape(Rectangle())
+//
+                    //Info
+                    InfoSection(geo: geo)
+                        .padding()
                 }
-                HStack{
-                    Text("Social")
-                    Text("Man")
-                }
-             
+            }
+        }
+    }
+}
+
+struct AboutMeSection: View{
+    let geo: GeometryProxy
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            let width = geo.size.width
+            let height = geo.size.height
+            
+            Text("About")
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width/4 , height: height/24)
+                .background(Color.gray)
+                .cornerRadius(65)
+                .opacity(0.7)
+            
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+}
+
+struct InterestSection: View{
+    let geo: GeometryProxy
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            let width = geo.size.width
+            let height = geo.size.height
+            
+            Text("Interest")
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width/4, height: height/24)
+                .background(Color.gray)
+                .cornerRadius(15)
+                .opacity(0.7)
+            
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+}
+
+struct NameAndAge: View{
+    var body: some View{
+        VStack{
+            HStack{
+                Text("Jane Doe")
+                    .bold()
+                    .font(.largeTitle)
                 
+                Text("25")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.gray)
             }
-            AsyncImage(url: URL(string: "https://i.pinimg.com/236x/af/1c/30/af1c30d6d881d9447dec06149f61d2f9--drawings-of-girls-anime-drawings-girl.jpg")) { image in
-                image.resizable()
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 600, height: 600)
-            .background(Color.gray)
-            .clipShape(Rectangle())
             
-            AsyncImage(url: URL(string: "https://i.pinimg.com/236x/af/1c/30/af1c30d6d881d9447dec06149f61d2f9--drawings-of-girls-anime-drawings-girl.jpg")) { image in
-                image.resizable()
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 600, height: 600)
-            .background(Color.gray)
-            .clipShape(Rectangle())
-
-            
+            Text("Server at Olive Garden")
+                .frame(alignment: .leading)
+                .font(.system(size: 15))
         }
     }
 }
@@ -93,8 +110,34 @@ struct ImageOverlay: View{
                 Text("blah blah")
                     .foregroundColor(.white)
             }.offset(x: -120, y: 260)
-       
+            
         }
+    }
+}
+
+struct InfoSection: View{
+    let geo: GeometryProxy
+    
+    var body: some View {
+            VStack(alignment: .leading, spacing: 50){
+                let width = geo.size.width
+                let height = geo.size.height
+                
+                HStack{
+                    NameAndAge()
+                    Spacer()
+                    
+                    Image("femenine")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(
+                            width: width/12,
+                            height: height/12,
+                            alignment: .center)
+                }
+                AboutMeSection(geo: geo)
+                InterestSection(geo: geo)
+            }
     }
 }
 
--- projectDate/views/screen/ProfileView.swift
+++ projectDate/views/screen/ProfileView.swift
@@ -16,84 +16,103 @@ struct ProfileView: View {
             }
         }
     }
-    struct CircleImageView: View{
-        var body: some View{
-            VStack{
-                AsyncImage(url: URL(string: "https://i.pinimg.com/236x/af/1c/30/af1c30d6d881d9447dec06149f61d2f9--drawings-of-girls-anime-drawings-girl.jpg"))
-                    .frame(width: 250, height: 250)
-                    .background(Color.gray)
-                    .clipShape(Circle())
-                    .shadow(radius: 10)
-                    .overlay(Circle().stroke(Color.blue, lineWidth: 3))
-                    .offset(y: -100)
+    
+}
+
+struct CircleImageView: View{
+    var body: some View{
+        VStack{
+            AsyncImage(url: URL(string: "https://i.pinimg.com/236x/af/1c/30/af1c30d6d881d9447dec06149f61d2f9--drawings-of-girls-anime-drawings-girl.jpg"))
+                .frame(width: 200, height: 200)
+                .background(Color.gray)
+                .clipShape(Circle())
+                .shadow(radius: 10)
+                .overlay(Circle().stroke(Color.blue, lineWidth: 10))
+                .offset(y: -100)
+                .padding(.top,30)
+                .padding(.bottom, 20)
+            
+            HStack{
                 
                 Text("dotZ3R0")
                     .bold()
                     .offset(y: -100)
+                    .font(.system(size: 25))
                 
-                
-                
-                NavigationLink(destination: PreviewProfileView()) {
-                    Text("PreviewProfile")
-                }.frame(width: 200, height: 50)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                
-                NavigationLink(destination: EditProfileView()){
-                    Text("Edit Profile")
-                }.frame(width: 200, height: 50)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                
-                
-                NavigationLink(destination: SettingsView()) {
-                    Text("Settings")
-                }.frame(width: 200, height: 50)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-                
+                Text("25")
+                    .bold()
+                    .offset(y: -100)
+                    .foregroundColor(Color.gray)
+                    .font(.system(size: 25))
             }
-            .navigationTitle("Profile")
-            .navigationBarTitleDisplayMode(.inline)
+            
+            Buttons()
         }
+        .navigationTitle("Profile")
+        .navigationBarTitleDisplayMode(.inline)
     }
-    
-    struct CustomHeader: View {
-        var body: some View {
-            Text("")
-                .frame(width: 200, height: 1000)
-                .background(Color.white)
-                .cornerRadius(65, corners: [.topRight])
-                .padding([.top, .trailing],200)
-            
-            
-            Text("")
-                .frame(width: 200, height: 1000)
-                .background(Color.blue)
-                .cornerRadius(65, corners: [.topLeft])
-                .padding([.top, .leading],200)
-            
-            
-            Text("")
-                .frame(width: 400, height: 1000)
-                .background(Color.white)
-                .cornerRadius(65, corners: [.topRight])
-                .padding([.top],350)
-            
-            
-            Text("")
-                .frame(width: 400, height: 200)
-                .background(Color.blue)
-                .cornerRadius(65, corners: [.topLeft, .topRight, .bottomLeft])
-                .padding([.bottom],850)
-            
-        }
-    }
-    
 }
+
+struct CustomHeader: View {
+    var body: some View {
+        Text("")
+            .frame(width: 200, height: 1000)
+            .background(Color.white)
+            .cornerRadius(65, corners: [.topRight])
+            .padding([.top, .trailing],200)
+        
+        
+        Text("")
+            .frame(width: 200, height: 1000)
+            .background(Color.blue)
+            .cornerRadius(65, corners: [.topLeft])
+            .padding([.top, .leading],200)
+        
+        
+        Text("")
+            .frame(width: 400, height: 1040)
+            .background(Color.white)
+            .cornerRadius(65, corners: [.topRight])
+            .padding([.top],350)
+        
+        
+        Text("")
+            .frame(width: 400, height: 160)
+            .background(Color.blue)
+            .cornerRadius(65, corners: [.topLeft, .topRight, .bottomLeft])
+            .padding([.bottom],850)
+        
+    }
+}
+
+struct Buttons: View {
+    var body: some View{
+        VStack{
+            NavigationLink(destination: PreviewProfileView()) {
+                Text("PreviewProfile")
+            }.frame(width: 300, height: 60)
+                .background(Color.black)
+                .foregroundColor(.white)
+                .cornerRadius(30)
+            
+            NavigationLink(destination: EditProfileView()){
+                Text("Edit Profile")
+            }.frame(width: 300, height: 60)
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .cornerRadius(30)
+            
+            
+            NavigationLink(destination: SettingsView()) {
+                Text("Settings")
+            }.frame(width: 300, height: 60)
+                .background(Color.black)
+                .foregroundColor(.white)
+                .cornerRadius(30)
+        }
+    }
+}
+
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
