a3a630156c209c093b946317a6a9888b6cfa0830
dotZ3R0 <43175805+drewe7192@users.noreply.github.com>
2023-05-06 13:50:57 -0400

    Add files via upload
    fresh file

---
+++ projectDate/feats/speedDate/models/SpeedDateModel.swift
@@ -0,0 +1,16 @@
+//
+//  speedDateModel.swift
+//  projectDate
+//
+//  Created by DotZ3R0 on 4/4/23.
+//
+
+import Foundation
+
+struct SpeedDateModel: Identifiable, Codable, Hashable{
+    var id: String
+    var roomId: String
+    var matchProfileIds: [String]
+    var eventDate: Date
+    var createdDate: Date
+}
