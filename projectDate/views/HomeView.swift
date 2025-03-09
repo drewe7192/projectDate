19f9ecbe1499402bf8a763da1e8cf5d8ee9f40ae
dotZ3R0 <drewe7192@gmail.com>
2025-03-09 06:37:41 -0400

    added logout button; tested new user signin

--- projectDate/views/HomeView.swift
+++ projectDate/views/HomeView.swift
@@ -14,6 +14,7 @@ struct HomeView: View {
     @State private var name: String = ""
     @State var timer: AnyCancellable?
     @State var isGuestJoining = false
+    @State var isSettingsView = false
     @EnvironmentObject var videoManager: VideoManager
     @EnvironmentObject var profileViewModel: ProfileViewModel
     
@@ -67,31 +68,48 @@ struct HomeView: View {
                     }
                 }
             }
-            
-            RoomView(isGuestJoining: self.$isGuestJoining)
-            
-            Text("Upcoming Events")
-                .bold()
-                .font(.system(size: 25))
-            ScrollView(.horizontal) {
-                
-                HStack{
-                    ForEach(1...3, id: \.self) {_ in
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 25)
-                                .fill(.gray)
-                                .opacity(0.3)
-                                .frame(width: 200, height: 200)
-                            VStack{
-                                Text("Title")
-                                Text("Event Date: Jan 3")
-                                
+            if isSettingsView {
+                SettingsView()
+            } else {
+                VStack{
+                    
+                    
+                    RoomView(isGuestJoining: self.$isGuestJoining)
+                    Button(action: {
+                        isSettingsView.toggle()
+                    }) {
+                        Text("Settingsview")
+                            .foregroundColor(.white)
+                            .padding(.horizontal)
+                            .padding(.vertical, 5)
+                            .background(Color.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                    
+                    Text("Upcoming Events")
+                        .bold()
+                        .font(.system(size: 25))
+                    ScrollView(.horizontal) {
+                        
+                        HStack{
+                            ForEach(1...3, id: \.self) {_ in
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .fill(.gray)
+                                        .opacity(0.3)
+                                        .frame(width: 200, height: 200)
+                                    VStack{
+                                        Text("Title")
+                                        Text("Event Date: Jan 3")
+                                        
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
-          
+           
         }
         .task {
             do {
