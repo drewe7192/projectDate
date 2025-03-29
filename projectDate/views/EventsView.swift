4fa1dd26309613c5f8f78fe8a83e54880cf8c0e1
dotZ3R0 <drewe7192@gmail.com>
2025-03-29 01:37:41 -0400

    Tabbar setup;

---
+++ projectDate/views/EventsView.swift
@@ -0,0 +1,18 @@
+//
+//  EventsView.swift
+//  ProjectDate
+//
+//  Created by DotZ3R0 on 3/28/25.
+//
+
+import SwiftUI
+
+struct EventsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsView()
+}
---
+++ projectDate/views/MessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesView.swift
+//  ProjectDate
+//
+//  Created by DotZ3R0 on 3/28/25.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessagesView()
+}
