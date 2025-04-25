8a144a5693c9e8dad2980464065cdbf2a48e86b9
dotZ3R0 <drewe7192@gmail.com>
2025-04-25 17:49:32 -0400

    delete some files not needed anymore

---
+++ projectDate/models/QuestionModel.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionModel.swift
+//  ProjectDate
+//
+//  Created by DotZ3R0 on 4/25/25.
+//
+
+import SwiftUI
+
+struct QuestionModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionModel()
+}
